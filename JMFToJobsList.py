1501549e1c517cca298f11275f6a6780b82bfdf3
Mica Funston <micafunston@gps-3905.regilmore.com>
2019-11-20 14:47:48 -0500

    Initial Commit

---
+++ JMFToJobsList.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python3
+# Tool for extracting a list of jobs from a "QueryQueueStatus" JMF response
+# Tested with Prinect, but should work with any JDF/JMF Device that has a queue
+
